427f8a859f085b3abf16ed1d636d78b1e86f2f8b
David Brochart <david.brochart@gmail.com>
2019-11-20 18:08:02 +0100

    Fix

--- src/Path.Transform.js
+++ src/Path.Transform.js
@@ -1,49 +1,3 @@
-/**
- * Leaflet vector features drag functionality
- * @author Alexander Milevski <info@w8r.name>
- * @preserve
- */
-
-/**
- * Matrix transform path for SVG/VML
- * Renderer-independent
- */
-L.Path.include({
-
-	/**
-	 * Applies matrix transformation to SVG
-	 * @param {Array.<Number>?} matrix
-	 */
-	_transform: function(matrix) {
-		if (this._renderer) {
-			if (matrix) {
-				this._renderer.transformPath(this, matrix);
-			} else {
-				// reset transform matrix
-				this._renderer._resetTransformPath(this);
-				this._update();
-			}
-		}
-		return this;
-	},
-
-	/**
-	 * Check if the feature was dragged, that'll supress the click event
-	 * on mouseup. That fixes popups for example
-	 *
-	 * @param  {MouseEvent} e
-	 */
-	_onMouseClick: function(e) {
-		if ((this.dragging && this.dragging.moved()) ||
-			(this._map.dragging && this._map.dragging.moved())) {
-			return;
-		}
-
-		this._fireMouseEvent(e);
-	}
-
-});
-
 
 
 /**
@@ -358,6 +312,7 @@ L.Handler.PathTransform = L.Handler.extend({
    * geometry of transformed layer
    */
   reset: function() {
+    this._rectShape = null;
     if (this._enabled) {
       if (this._rect) {
         this._handlersGroup.removeLayer(this._rect);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-require('./SVG');
-require('./SVG.VML');
-require('./Canvas');
-require('./Path.Transform');
+var L = require('leaflet');
 require('./Path.Drag');
 require('./Util');
 require('./Matrix');
