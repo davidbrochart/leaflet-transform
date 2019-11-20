39f2903080cc5c20dc234e7d9af8c48d823ae46f
David Brochart <david.brochart@gmail.com>
2019-11-20 15:03:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# leaflet-transform
+
+Drag/rotate/resize handler for Leaflet vector features.
+
+**This project is a fork of the Leaflet.Path.Transform plugin**
+
+### License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('./SVG');
+require('./SVG.VML');
+require('./Canvas');
+require('./Path.Transform');
+require('./Path.Drag');
+require('./Util');
+require('./Matrix');
+require('./Path.Transform');
